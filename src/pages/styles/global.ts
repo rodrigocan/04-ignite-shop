c718bce99bb8213ef0639f28983c6f2dbf082e75
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-01-12 19:52:07 -0300

    Estilos globais

--- src/pages/styles/global.ts
+++ src/pages/styles/global.ts
@@ -5,4 +5,15 @@ export const globalStyles = globalCss({
     margin: 0,
     padding: 0,
   },
+
+  body: {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+    'webkit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+  },
 })
