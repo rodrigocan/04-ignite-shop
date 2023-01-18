f38c4fd9d8989ac9814e29c6f81086ad65b7f761
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-01-18 09:40:05 -0300

    Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from './styles/pages/success'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Rodrigo Cândido</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
